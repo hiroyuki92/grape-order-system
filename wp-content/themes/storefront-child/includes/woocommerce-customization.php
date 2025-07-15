652962e4ec9ae695f5e46a9ebb120dba3725e883
yukiko <yukiko@magnolia.co.jp>
2025-07-15 23:10:59 +0900

    メールアドレスを非表示

--- wp-content/themes/storefront-child/includes/woocommerce-customization.php
+++ wp-content/themes/storefront-child/includes/woocommerce-customization.php
@@ -25,3 +25,22 @@ function change_billing_text_to_sender($translated_text, $text, $domain) {
     }
     return $translated_text;
 }
+
+// 請求先（送り主）のメールアドレスフィールドを非表示
+add_filter('woocommerce_checkout_fields', 'hide_billing_email_field');
+function hide_billing_email_field($fields) {
+    unset($fields['billing']['billing_email']);
+    return $fields;
+}
+
+// マイアカウントの住所ページでもメールアドレスフィールドを非表示
+add_filter('woocommerce_billing_fields', 'hide_billing_email_field_myaccount');
+add_filter('woocommerce_shipping_fields', 'hide_shipping_email_field_myaccount');
+function hide_billing_email_field_myaccount($fields) {
+    unset($fields['billing_email']);
+    return $fields;
+}
+function hide_shipping_email_field_myaccount($fields) {
+    unset($fields['shipping_email']);
+    return $fields;
+}
